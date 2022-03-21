e250018ec594f22c97836a7c00851b02d281647f
mikehemli <mikehemli@gmail.com>
2022-03-21 13:57:39 +0200

    Adding a flask application

--- main_scores.py
+++ main_scores.py
@@ -1,4 +1,18 @@
-def score_server():
-    f = open('scores.txt', 'r')
-    number = int(f.read())
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/score")
+def score_server():
+    try:
+        with open("scores.txt") as f:
+            score = f.readline()
+            return render_template("score.html", SCORE=" " + score)
+    except BaseException as ex:
+        print(ex)
+        return render_template("error.html", ERROR='There was an error while loading this page...')
+
+
+app.run(port=8123, debug=True)
