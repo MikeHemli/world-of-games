0aef61658fb94ba8b31f8596b221fd69023bd897
mikehemli <42358198+MikeHemli@users.noreply.github.com>
2022-04-03 13:57:05 +0300

    Update e2e.py

--- e2e.py
+++ e2e.py
@@ -16,7 +16,7 @@ chromeOptions.headless = True
 # chrome_driver = webdriver.Chrome(
 #    chrome_options=chromeOptions, executable_path="/usr/bin/chromedriver")
 # chrome_driver = webdriver.Chrome(chrome_options=chromeOptions)
-chrome_driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver')
+chrome_driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
 
 
 def test_scores_service(url):
