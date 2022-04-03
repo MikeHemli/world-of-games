d9b55e6cdce39f612f93f817b465abadc1e3dcea
Michael Hemli <9804081-mikehemli@users.noreply.gitlab.com>
2022-04-03 13:40:26 +0300

    modifying file

--- e2e.py
+++ e2e.py
@@ -13,7 +13,8 @@ chromeOptions.add_argument("--disable-extensions")
 chromeOptions.add_argument(r"user-data-dir=.\cookies\\test")
 chromeOptions.headless = True
 # chrome_driver = webdriver.Chrome(ChromeDriverManager().install())
-chrome_driver = webdriver.Chrome(chrome_options=Options, executable_path="/usr/bin/chromedriver")
+chrome_driver = webdriver.Chrome(
+    chrome_options=chromeOptions, executable_path="/usr/bin/chromedriver")
 # browser = webdriver.Chrome(chrome_options=options, executable_path="/usr/local/bin/chromedriver")
 
 
